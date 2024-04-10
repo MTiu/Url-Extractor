06e4c20361000dc043e7c2908e693aebce3aa558
Matthew-Tiu <mattjtiu@gmail.com>
2024-04-10 21:18:17 +0800

    UrlExtractor Init Commit

--- lib/urlextractor.rb
+++ lib/urlextractor.rb
@@ -3,6 +3,29 @@
 require_relative "urlextractor/version"
 
 module Urlextractor
-  class Error < StandardError; end
-  # Your code goes here...
+    class Error < StandardError; end
+        # Your code goes here...
+    def self.protocol input
+        output = ""
+        output = input[0, input.index(':')]
+        output
+    end
+    
+    def self.domain(url)
+        url.match(/(?:https?:\/\/)?(?:www\.)?([^\/]+)/)[1] || ''
+    end
+    
+    def self.extension(url)
+        domain = self.domain(url)
+        domain.split('.').last || ''
+    end
+    
+    def self.path(url)
+        url.match(/https?:\/\/(?:www\.)?.+?(\/.*)/)[1] || ''
+    end
+    
+    def self.query_params(url)
+        query_string = url.match(/\?(.*)/)[1]
+        query_string.split('&').map { |pair| pair.split('=') }.to_h
+    end
 end
--- spec/urlextractor_spec.rb
+++ spec/urlextractor_spec.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
+require "spec_helper"
 
 RSpec.describe Urlextractor do
-  it "has a version number" do
-    expect(Urlextractor::VERSION).not_to be nil
-  end
+    it "extracts the protocol part of the string url" do 
+        expect(Urlextractor.protocol "https://hackerhero.com").to eq("https")
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+    it "extracts the domain part of the string url" do
+        expect(Urlextractor.domain "https://hackerhero.com").to eq("hackerhero.com")
+        expect(Urlextractor.domain "https://hackerhero.com/groups").to eq("hackerhero.com")
+    end
+
+    it "extracts the extension" do
+        expect(Urlextractor.extension "https://hackerhero.com").to eq("com")
+    end
+
+    it "extracts the remaining parts after domain" do
+        expect(Urlextractor.path "https://www.hackerhero.com/groups/abc").to eq("/groups/abc")
+        expect(Urlextractor.path "https://www.hackerhero.com/groups/abc/index.html").to eq("/groups/abc/index.html")
+    end
+
+    it "gets key-value pairs after the path" do
+        expect(Urlextractor.query_params "https://www.hackerhero.com/groups?key1=value1&key2=value2").to eq({"key1"=>"value1", "key2"=>"value2"})
+    end
 end
